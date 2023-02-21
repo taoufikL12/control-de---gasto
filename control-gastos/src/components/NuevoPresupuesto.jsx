d9b0008912cb0ca749bc2361c3886e7335191aa9
mensitaoufik <mensitaoufik29@gmail.com>
2023-02-21 18:35:35 +0100

    primer commit

---
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import Mensaje from './Mensaje'
+
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+       if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto valido')
+       } else {
+            console.log('Si es un presupuesto valido')   
+       }
+        
+    }
+
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+
+        <form onSubmit={handlePresupuesto}  className="formulario">
+
+            <div className="campo">
+
+                <label>Definir Presupuesto</label>
+
+                <input 
+                
+                    className="nuevo-presupuesto"
+                    type="text"
+                    placeholder="Añade tu Presupuesto"
+                    value={presupuesto}
+                    onChange={  e => setPresupuesto(e.target.value) }
+                
+                />
+
+            </div>
+
+            <input type="submit" value="Añadir" />
+
+            {mensaje && <Mensaje tipo="error" >{mensaje}</Mensaje>}
+
+
+        </form>
+
+
+
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
